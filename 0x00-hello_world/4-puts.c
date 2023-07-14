279a346b33b6ed0b9746b2ca7c6aec7a2356e978
root <mstf74>
2023-07-13 18:58:32 -0700

    try

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main - Entry point 
- * 
- * Description : A C programme that prints with puts function 
- * 
- * Return : always 0 (success)
-*/
-int main()
+ * main - Entry point
+ *
+ * Description: A C program that prints with puts function.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	 return (0);
+	return (0);
 }
