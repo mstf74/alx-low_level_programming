93b41a4db3118dffc0102c707211c3512b60970d
root <mstf74>
2023-07-13 18:01:45 -0700

    task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point 
+ * 
+ * Description : A C programme that prints with puts function 
+ * 
+ * Return : always 0 (success)
+*/
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	 return (0);
+}
