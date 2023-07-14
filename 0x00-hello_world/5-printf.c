7bc6016a5be7cf670dcb65b0c95daad95affe7c8
root <mstf74>
2023-07-13 19:08:08 -0700

    task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
 /**
- * main - Entry point 
- * 
- * Description : A C program that prints with printf function. 
- * 
- * Return: always 0 (success)
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function.
+ *
+ * Return: Always 0 (success)
 */
 
 int main(void)
